f9eeb9b389d0095d29c90fd4e026d890a36668f9
nik1olay <85620548+nik1olay@users.noreply.github.com>
2021-08-06 14:04:31 +0300

    Initial Commit

---
+++ l6hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  l6hw
+//
+//  Created by Cергей Иванович on 6.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
